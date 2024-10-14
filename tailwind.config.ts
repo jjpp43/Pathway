3cc4c99d517db5448da6ab0ef095965963035aaa
Jazzy <jparkdev63@gmail.com>
2024-10-14 15:30:15 -0400

    Font weight is enabled

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	fontWeight: {
+		thin: '100',
+		hairline: '100',
+		extralight: '200',
+		light: '300',
+		normal: '400',
+		medium: '500',
+		semibold: '600',
+		bold: '700',
+		extrabold: '800',
+		black: '900',
+	  },
   	extend: {
 		fontFamily: {
 			mono: ['var(--font-montserrat)']
